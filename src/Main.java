d8fb5c493c2248e8f1fce6d819687a0034e5f799
Adam Krawiec <adamrumia@wp.pl>
2024-01-10 19:46:29 +0100

    przed pierwszą pętlą

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swój wynik: ");
+        int score = scanner.nextInt();
+        System.out.println();
+        System.out.println("Twój wynik to: " + score + "%.");
 
     }
 
