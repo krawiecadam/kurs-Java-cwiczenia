26c7d64cf03fbb10c303953eed070003160fd2e3
Adam Krawiec <adamrumia@wp.pl>
2024-01-10 20:04:57 +0100

    Klasyfikacja ocen - podaj wynik tylko z zakresu 0-100

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int score = 10;
+
         System.out.println("Podaj swój wynik: ");
-        int score = scanner.nextInt();
-        System.out.println();
+            score = scanner.nextInt();
+        while (score > 100 || score <0) {
+
+            System.out.println("Podałeś wynik spoza zakresu 0-100. Podaj wynik jeszcze raz:");
+            score = scanner.nextInt();
+        }
         System.out.println("Twój wynik to: " + score + "%.");
-        if (score >= 90 && score <=100) {
+        if (score >= 90) {
             System.out.println("Twoja ocena to: 5. Gratulacje !");
-        } else if ((score >= 80 && score <=89)) {
+        } else if ((score >= 80 )) {
             System.out.println("Twoja ocena to: 4. Not bad ;)");
 
-        } else if ((score >= 70 && score <=79)) {
+        } else if ((score >= 70 )) {
             System.out.println("Twoja ocena to: 3. Tak sobie...");
-        } else if ((score >= 60 && score <=69)) {
+        } else if ((score >= 60 )) {
             System.out.println("Twoja ocena to: 2. Oj słabiutko...");
-        } else if ((score <= 59)) {
+        } else  {
             System.out.println("Twoja ocena to: 1. Do widzenia.");
         }
     }
