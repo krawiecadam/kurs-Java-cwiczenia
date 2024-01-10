8e9f53db60b76cc76adc0798044edbe008ad520b
Adam Krawiec <adamrumia@wp.pl>
2024-01-10 19:52:20 +0100

    Klasyfikacja ocen - działa.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,18 @@ public class Main {
         int score = scanner.nextInt();
         System.out.println();
         System.out.println("Twój wynik to: " + score + "%.");
+        if (score >= 90 && score <=100) {
+            System.out.println("Twoja ocena to: 5. Gratulacje !");
+        } else if ((score >= 80 && score <=89)) {
+            System.out.println("Twoja ocena to: 4. Not bad ;)");
 
+        } else if ((score >= 70 && score <=79)) {
+            System.out.println("Twoja ocena to: 3. Tak sobie...");
+        } else if ((score >= 60 && score <=69)) {
+            System.out.println("Twoja ocena to: 2. Oj słabiutko...");
+        } else if ((score <= 59)) {
+            System.out.println("Twoja ocena to: 1. Do widzenia.");
+        }
     }
 
 }
