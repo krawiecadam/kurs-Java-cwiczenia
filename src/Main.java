4d772866db33ad79204e7288bf6c482e5b050247
Adam Krawiec <adamrumia@wp.pl>
2024-01-26 20:21:14 +0100

    Palindrom - moja wersja działa

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,41 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-       ArrayList<String> chart = new ArrayList<>();
+       ArrayList<String> tablicaZLiterkami = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
+        text = text.toUpperCase();
         int dlugoscWyrazu = text.length();
         String letter;
         String pierwszaLiterka;
         String ostatniaLiterka;
-        int prawda=-1;
+        int zgodne=-1;
         char a;
 
         for (int i=0; i<text.length(); i++) {
              a = text.charAt(i);
              String b = Character.toString(a);
-             chart.add(b);
+             tablicaZLiterkami.add(b);
         }
 
         for (int i1=0; i1<=((text.length()/2)); i1++) {
             dlugoscWyrazu--;
-        pierwszaLiterka = chart.get(i1);
-        ostatniaLiterka = chart.get((dlugoscWyrazu));
+        pierwszaLiterka = tablicaZLiterkami.get(i1);
+        ostatniaLiterka = tablicaZLiterkami.get((dlugoscWyrazu));
         if (!(pierwszaLiterka.equals(ostatniaLiterka))) {
             break;
         } else {
-            prawda++;
+            zgodne++;
         }
 }
 
-        if ((text.length()/2) == prawda) {
-            System.out.println("ok");
+        if ((text.length()/2) == zgodne) {
+            System.out.println();
+            System.out.println(text + " to palindrom ;)");
 
         } else {
-            System.out.println("nie ok");
+            System.out.println();
+            System.out.println(text + " to nie palindrom :(");
         }
 
 
